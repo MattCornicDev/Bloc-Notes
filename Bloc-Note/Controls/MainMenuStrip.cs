cb8423625db54ab2cb3db53897c13d1a7f3ab599
MattCornicDev <glgharib@gmail.com>
2021-03-18 20:03:22 +0100

    premiere étape fenetre

---
+++ Bloc-Note/Controls/MainMenuStrip.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bloc_Note.Controls
+{
+    class MainMenuStrip : MenuStrip
+    {
+        public MainMenuStrip()
+        {
+            Name = "MainMenuStrip";
+            Dock = DockStyle.Top;
+
+            FileDropDownMenu();
+            EditDropDownMenu();
+            FormatDropDownMenu();
+            ViewDropDownMenu();
+
+        }
+
+        public void FileDropDownMenu()
+        {
+            var fileDropDownMenu = new ToolStripMenuItem("Fichier");
+
+            var newMenu = new ToolStripMenuItem("Nouveau", null, null, Keys.Control | Keys.N);
+            var openMenu = new ToolStripMenuItem("Ouvrir...", null, null, Keys.Control | Keys.O);
+            var saveMenu = new ToolStripMenuItem("Enregister", null, null, Keys.Control | Keys.S);
+            var saveAsMenu = new ToolStripMenuItem("Enregister sous...", null, null, Keys.Control | Keys.Shift | Keys.N);
+            var quiteMenu = new ToolStripMenuItem("Quitter", null, null, Keys.Alt | Keys.F4);
+
+            fileDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { newMenu, openMenu, saveMenu, saveAsMenu, quiteMenu });
+
+            Items.Add(fileDropDownMenu);
+        }
+
+        public void EditDropDownMenu()
+        {
+            var EditDropDownMenu = new ToolStripMenuItem("Edition");
+
+            var cancelMenu = new ToolStripMenuItem("Annuler", null, null, Keys.Control | Keys.Z);
+            var restoreMenu = new ToolStripMenuItem("Restaurer...", null, null, Keys.Control | Keys.Y);
+
+
+            EditDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { cancelMenu, restoreMenu });
+
+            Items.Add(EditDropDownMenu);
+        }
+
+        public void FormatDropDownMenu()
+        {
+            var FormatDropDownMenu = new ToolStripMenuItem("Format");
+
+            var fontMenu = new ToolStripMenuItem("Police...");
+
+
+            FormatDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { fontMenu });
+
+            Items.Add(FormatDropDownMenu);
+        }
+
+        public void ViewDropDownMenu()
+        {
+            var ViewDropDownMenu = new ToolStripMenuItem("Affichage");
+
+            var alwaysOnTopMenu = new ToolStripMenuItem("Toujours devant");
+
+            var zoomDropDownMenu = new ToolStripMenuItem("Zomm");
+
+            var zoomInMenu = new ToolStripMenuItem("Zoom avant", null, null, Keys.Control | Keys.Add);
+            var zoomOutMenu = new ToolStripMenuItem("Zoom arrière", null, null, Keys.Control | Keys.Subtract);
+            var zoomResetMenu = new ToolStripMenuItem("Restaurer le zoom par défaut", null, null, Keys.Control | Keys.Divide);
+
+            zoomInMenu.ShortcutKeyDisplayString = "Ctrl+Num +";
+            zoomOutMenu.ShortcutKeyDisplayString = "Ctrl+Num -";
+            zoomResetMenu.ShortcutKeyDisplayString = "Ctrl+Num /";
+
+            zoomDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { zoomInMenu, zoomOutMenu, zoomResetMenu });
+
+            ViewDropDownMenu.DropDownItems.AddRange(new ToolStripItem[] { alwaysOnTopMenu, zoomDropDownMenu });
+
+            Items.Add(ViewDropDownMenu);
+        }
+    }
+}
---
+++ Bloc-Note/MainForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Bloc_Note
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1441, 755);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bloc Note";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
